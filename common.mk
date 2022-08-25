18fa8df8b5b2ccedd5d32d2e3455a36c95419d30
Tim Zimmermann <tim@linux4.de>
2022-08-25 10:07:10 +0200

    m30s-common: Switch to AIDL sensors HAL
    Change-Id: I909510578145057be69041c71836e0a3b436f7b7

--- common.mk
+++ common.mk
@@ -229,7 +229,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.samsung-multihal
+    android.hardware.sensors-service.samsung-multihal
+
 
 # Shims
 PRODUCT_PACKAGES += \
